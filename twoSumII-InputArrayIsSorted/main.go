22a2bdfbe0fd3f4f5f2bc3ed5f41cd56849d2130
Ayoub RABYA <rabiyaeayoub@gmail.com>
2024-07-20 18:10:00 +0100

    add two sumII

--- twoSumII-InputArrayIsSorted/main.go
+++ twoSumII-InputArrayIsSorted/main.go
@@ -4,18 +4,17 @@ import "fmt"
 
 func twoSum(numbers []int, target int) []int {
 	for i, num := range numbers {
-		// needNum := target - num
-
 		low, heigh := 0, len(numbers)-1
 		mid := (low + heigh) / 2
+		needNum := target - num
 
 		for low <= heigh {
 			if numbers[mid]+num == target && mid != i {
 				return []int{i + 1, mid + 1}
-			} else if numbers[mid] >= target {
+			} else if numbers[mid] > needNum {
 				heigh = mid - 1
 				mid = (low + heigh) / 2
-			} else if numbers[mid] <= target {
+			} else if numbers[mid] <= needNum {
 				low = mid + 1
 				mid = (low + heigh) / 2
 			}
@@ -27,8 +26,8 @@ func twoSum(numbers []int, target int) []int {
 }
 
 func main() {
-	numbers := []int{-1, 0}
-	target := -1
+	numbers := []int{1, 2, 3, 4, 4, 9, 56, 90}
+	target := 8
 
 	fmt.Println(twoSum(numbers, target))
 }
